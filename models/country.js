4f3d5645fc168a7becf7c44d8372cbe7fa90d238
Muhammad Fazil Qureshi <softmfq@gmail.com>
2018-12-05 22:38:15 +0500

    fixed eslint issuse

--- models/country.js
+++ models/country.js
@@ -1,9 +1,13 @@
-'use strict';
+
 module.exports = (sequelize, DataTypes) => {
-  const Country = sequelize.define('Country', {
-    name: DataTypes.STRING
-  }, {});
-  Country.associate = function(models) {
+  const Country = sequelize.define(
+    'Country',
+    {
+      name: DataTypes.STRING,
+    },
+    {},
+  );
+  Country.associate = function (models) {
     // associations can be defined here
   };
   return Country;
