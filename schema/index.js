f1dbc379aab64b01f65c9276ea21ce806a4dc6f8
Muhammad Fazil Qureshi <softmfq@gmail.com>
2018-12-05 22:41:02 +0500

    update schema/index with new content

--- schema/index.js
+++ schema/index.js
@@ -1,9 +1,13 @@
 const { gql } = require('apollo-server-express');
 // const { userSchema } = require('./userSchema');
+const { countrySchema, countryQuerySchema } = require('./countrySchema');
 
 const typeDefs = gql`
+  ${countrySchema}
+
   type Query {
     hello: String
+    ${countryQuerySchema}
   }
 `;
 
