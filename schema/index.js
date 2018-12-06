bedeef16d5c8b1654b129516aa2e839b402b8a09
Muhammad Fazil Qureshi <softmfq@gmail.com>
2018-12-06 11:52:46 +0500

    added company schema

--- schema/index.js
+++ schema/index.js
@@ -1,13 +1,16 @@
 const { gql } = require('apollo-server-express');
 // const { userSchema } = require('./userSchema');
 const { countrySchema, countryQuerySchema } = require('./countrySchema');
+const { companySchema, companyQuerySchema } = require('./companySchema');
 
 const typeDefs = gql`
   ${countrySchema}
+  ${companySchema}
 
   type Query {
     hello: String
     ${countryQuerySchema}
+    ${companyQuerySchema}
   }
 `;
 
