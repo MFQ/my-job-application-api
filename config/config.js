62750a2d5edc7f165916f0147b0b0c90023abd23
Muhammad Fazil Qureshi <softmfq@gmail.com>
2018-12-27 19:40:54 +0500

    change configuration of sequlize

---
+++ config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    username: null,
+    password: null,
+    database: 'job_application_development',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  },
+  test: {
+    username: 'root',
+    password: null,
+    database: 'job_application_test',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
+    dialect: 'postgres',
+  },
+};
